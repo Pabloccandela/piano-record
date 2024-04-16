e2cf6dabbc6c65eff568a03d0e50e911d2f0687e
Pabloccandela <pabloccandela@gmail.com>
2024-04-15 21:49:48 -0300

    piano funcional

--- src/components/pulse.jsx
+++ src/components/pulse.jsx
@@ -1,43 +1,86 @@
-import { useRef } from "react"
+import { useState } from "react"
 import { useEffect } from "react"
+import * as Tone from 'tone'
 import "./tecla.css"
 export const Pulse = () => {
-    const teclas = useRef([])
+
+    const [teclas, setTeclas] = useState([])
+    const synth = new Tone.Synth().toDestination();
+    const notas = [
+      {
+        key: "a",
+        cifrado: "C4",
+        nota: "Do"
+      },
+      {
+        key: "s",
+        cifrado: "D4",
+        nota: "Re"
+      },
+      {
+        key: "d",
+        cifrado: "E4",
+        nota: "Mi"
+      },
+      {
+        key: "f",
+        cifrado: "F4",
+        nota: "Fa"
+      },
+      {
+        key: "g",
+        cifrado: "G4",
+        nota: "Sol"
+      },
+      {
+        key: "h",
+        cifrado: "A4",
+        nota: "La"
+      },
+      {
+        key: "j",
+        cifrado: "B4",
+        nota: "Si"
+      }
+    ];
+    
     useEffect(() => {
         const handleKeyDown = (event) => {
-          if (!teclas.current.includes(event.key)) {
-            console.log(!teclas.current.includes(event.key));
-            teclas.current.push(event.key);
-            console.log(teclas.current);
+          let nota = notas.find((nota)=>nota.key==event.key);
+          if (nota && !teclas.includes(event.key)) {
+            setTeclas([...teclas, event.key]);
+            synth.triggerAttackRelease(nota.cifrado, "8n");
           }
         };
     
         const handleKeyUp = (event) => {
-          const index = teclas.current.indexOf(event.key); // Find index of released key
-          if (index !== -1) {
-            teclas.current.splice(index, 1); // Remove released key from array
-            console.log(teclas.current); // Log when a key is released
+          if(teclas.includes(event.key)){
+            setTeclas(teclas.filter((tecla)=>tecla!=event.key))
+            console.log(teclas);
           }
         };
     
-        // Add event listeners on component mount
         window.addEventListener('keydown', handleKeyDown);
         window.addEventListener('keyup', handleKeyUp);
     
-        // Clean up function to remove event listeners on component unmount
         return () => {
             window.removeEventListener('keydown', handleKeyDown);
             window.removeEventListener('keyup', handleKeyUp);
         };
-    }, [teclas.current]);
+    }, [teclas]);
 
     return (
-        <div className="teclado">
-            <div className={"tecla " + teclas.current.includes("a")?"pulsar":""}></div>
-            <div className={"tecla " + teclas.current.includes("b")?"pulsar":""}></div>
-            <div className={"tecla " + teclas.current.includes("c")?"pulsar":""}></div>
-            <div className={"tecla " + teclas.current.includes("d")?"pulsar":""}></div>
-            <div className={"tecla " + teclas.current.includes("e")?"pulsar":""}></div>
+        <main>
+          <div className="teclado">
+            <div className={teclas.includes("a")?"tecla pulsar":"tecla"}>DO</div>
+            <div className={teclas.includes("s")?"tecla pulsar":"tecla"}>RE</div>
+            <div className={teclas.includes("d")?"tecla pulsar":"tecla"}>MI</div>
+            <div className={teclas.includes("f")?"tecla pulsar":"tecla"}>FA</div>
+            <div className={teclas.includes("g")?"tecla pulsar":"tecla"}>SOL</div>
+            <div className={teclas.includes("h")?"tecla pulsar":"tecla"}>LA</div>
+            <div className={teclas.includes("j")?"tecla pulsar":"tecla"}>SI</div>
         </div>
+        
+      </main>
     )
 }
